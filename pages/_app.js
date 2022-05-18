b676a216d25b44914262b68fa66ffef9f03f91ba
yunsung <yunsung@marvrus.com>
2022-05-18 18:03:12 +0900

    샘플 프로젝트 진행 완료

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { CacheProvider } from "@emotion/react";
+import createEmotionCache from "../styles/createEmotionCache";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+import theme from "../styles/theme";
+
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+
+export default function MyApp(props) {
+  const clientSideEmotionCache = createEmotionCache();
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>처음 만들어보는 Next.js + Mui5</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
-
-export default MyApp
